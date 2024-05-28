b19cb3da139b2f612fb0364700cd3f9eddfe74a9
vinhngle <vinhngle@users.noreply.github.com>
2024-05-28 23:02:58 +0700

    Update omniauth-microsoft365.rb

--- lib/omniauth-microsoft365.rb
+++ lib/omniauth-microsoft365.rb
@@ -9,10 +9,10 @@ module OmniAuth
 
       DEFAULT_SCOPE = "openid email profile https://graph.microsoft.com/User.Read"
 
-      option :client_options,
-             site: "https://login.microsoftonline.com",
-             authorize_url: "/common/oauth2/v2.0/authorize",
-             token_url: "/common/oauth2/v2.0/token"
+      #option :client_options,
+      #       site: "https://login.microsoftonline.com",
+      #       authorize_url: "/common/oauth2/v2.0/authorize",
+      #       token_url: "/common/oauth2/v2.0/token"
 
       option :authorize_options, [:scope]
 
